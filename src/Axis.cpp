6bfea7113197eb53b0eea133969716190afe2caf
xubury <xubury97@gmail.com>
2020-12-24 18:45:35 +0800

    - update compile settings

--- src/Axis.cpp
+++ src/Axis.cpp
@@ -7,13 +7,10 @@ Axis::Axis(const glm::mat4 &model) : RenderTarget(model), m_VAO(0) {
     // draw X, Y, Z lines with Red, Green, Blue colors
     // note that the first three columns are the point position
     // the last three columnn are the colors
-    float vertices[] = {
-        0.f, 0.f, 0.f, 1.f, 0.f, 0.f,
-        1.f, 0.f, 0.f, 1.f, 0.f, 0.f,
-        0.f, 0.f, 0.f, 0.f, 1.f, 0.f,
-        0.f, 1.f, 0.f, 0.f, 1.f, 0.f,
-        0.f, 0.f, 0.f, 0.f, 0.f, 1.f,
-        0.f, 0.f, 1.f, 0.f, 0.f, 1.f};
+    float vertices[] = {0.f, 0.f, 0.f, 1.f, 0.f, 0.f, 1.f, 0.f, 0.f,
+                        1.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f, 0.f,
+                        0.f, 1.f, 0.f, 0.f, 1.f, 0.f, 0.f, 0.f, 0.f,
+                        0.f, 0.f, 1.f, 0.f, 0.f, 1.f, 0.f, 0.f, 1.f};
     glGenVertexArrays(1, &m_VAO);
     glBindVertexArray(m_VAO);
 
@@ -26,22 +23,25 @@ Axis::Axis(const glm::mat4 &model) : RenderTarget(model), m_VAO(0) {
     // in file axis.vs, there is a statement
     // layout (location = 0) in vec3 vertex;
     // the first argument 0 means (location = 0)
-    glVertexAttribPointer(0,                 // location = 0 in vertex shader file
-                          3,                 // the position has X,Y,Z 3 elements
-                          GL_FLOAT,          // element type
-                          GL_FALSE,          // do not normalize
-                          6 * sizeof(float), // Stride
-                          (void *) nullptr); // an offset of into the array, it is 0
+    glVertexAttribPointer(
+        0,                  // location = 0 in vertex shader file
+        3,                  // the position has X,Y,Z 3 elements
+        GL_FLOAT,           // element type
+        GL_FALSE,           // do not normalize
+        6 * sizeof(float),  // Stride
+        (void *)nullptr);   // an offset of into the array, it is 0
     glEnableVertexAttribArray(1);
     // in file axis.vs, there is a statement
     // layout (location = 1) in vec3 color;
     // the first argument 1 means (location = 1)
-    glVertexAttribPointer(1,                 // location = 1 in vertex shader file
-                          3,                 // the color has RGB 3 elements
-                          GL_FLOAT,          // element type
-                          GL_FALSE,          // do not normalize
-                          6 * sizeof(float), // Stride
-                          (void *) (3 * sizeof(float))); // an offset of into the array, it is 3 * sizeof(float)
+    glVertexAttribPointer(
+        1,                             // location = 1 in vertex shader file
+        3,                             // the color has RGB 3 elements
+        GL_FLOAT,                      // element type
+        GL_FALSE,                      // do not normalize
+        6 * sizeof(float),             // Stride
+        (void *)(3 * sizeof(float)));  // an offset of into the array, it is 3 *
+                                       // sizeof(float)
     // color columns are behind the XYZ columns
 
     glBindVertexArray(0);
@@ -54,4 +54,3 @@ void Axis::DrawArray(const Shader &shader) {
     glDrawArrays(GL_LINES, 0, 6);
     glBindVertexArray(0);
 }
-
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,9 +1,14 @@
 #include "Camera.hpp"
 
-Camera::Camera(const glm::vec3 &position, const glm::vec3 &front, const glm::vec3 &up)
-    : m_position(position), m_front(front), m_up(up), m_zoom(ZOOM),
-      m_screenWidth(0), m_screenHeight(0), m_type(Type::PERSPECTIVE) {
-}
+Camera::Camera(const glm::vec3 &position, const glm::vec3 &front,
+               const glm::vec3 &up)
+    : m_position(position),
+      m_front(front),
+      m_up(up),
+      m_zoom(ZOOM),
+      m_screenWidth(0),
+      m_screenHeight(0),
+      m_type(Type::PERSPECTIVE) {}
 
 glm::mat4 Camera::GetViewMatrix() const {
     return glm::lookAt(m_position, m_position + m_front, m_up);
@@ -18,35 +23,43 @@ void Camera::ProcessMouseScroll(float yoffset) {
     }
 }
 
-
 glm::mat4 Camera::GetProjection() const {
     if (m_type == Type::PERSPECTIVE)
-        return glm::perspective(glm::radians(GetFOV()), (float) m_screenWidth / (float) m_screenHeight, 0.1f, 100.0f);
+        return glm::perspective(glm::radians(GetFOV()),
+                                (float)m_screenWidth / (float)m_screenHeight,
+                                0.1f, 100.0f);
     else {
         glm::mat4 view = GetViewMatrix();
         float ratio_size_per_depth = atan(glm::radians(GetFOV() / 2.0f)) * 2.0f;
-        float xFactor = (float) m_screenWidth / (float) (m_screenWidth + m_screenHeight) * abs(view[3][2]) *
-            ratio_size_per_depth;
-        float yFactor = (float) m_screenHeight / (float) (m_screenWidth + m_screenHeight) * abs(view[3][2]) *
-            ratio_size_per_depth;
+        float xFactor = (float)m_screenWidth /
+                        (float)(m_screenWidth + m_screenHeight) *
+                        abs(view[3][2]) * ratio_size_per_depth;
+        float yFactor = (float)m_screenHeight /
+                        (float)(m_screenWidth + m_screenHeight) *
+                        abs(view[3][2]) * ratio_size_per_depth;
         return glm::ortho(-xFactor, xFactor, -yFactor, yFactor, 0.1f, 100.0f);
     }
 }
 
-glm::mat4 Camera::CalculateRotateFromView(float x_offset, float y_offset) const {
+glm::mat4 Camera::CalculateRotateFromView(float x_offset,
+                                          float y_offset) const {
     glm::mat4 rotation(1);
     glm::mat4 view = GetViewMatrix();
-    rotation = glm::rotate(rotation, glm::radians((float) x_offset), glm::vec3(view[1]));
-    rotation = glm::rotate(rotation, glm::radians((float) -y_offset), glm::vec3(view[0]));
+    rotation = glm::rotate(rotation, glm::radians((float)x_offset),
+                           glm::vec3(view[1]));
+    rotation = glm::rotate(rotation, glm::radians((float)-y_offset),
+                           glm::vec3(view[0]));
     return rotation;
 }
 
-glm::mat4 Camera::CalculateTranslateFromView(float x_offset, float y_offset) const {
+glm::mat4 Camera::CalculateTranslateFromView(float x_offset,
+                                             float y_offset) const {
     glm::mat4 projection = GetProjection();
     glm::mat4 view = GetViewMatrix();
-    float xOffset_clip = (2 * x_offset) / (float) m_screenWidth;
-    float yOffset_clip = (2 * y_offset) / (float) m_screenHeight;
-    glm::mat4 transform = glm::translate(glm::mat4(1.0), glm::vec3(xOffset_clip, yOffset_clip, 0));
+    float xOffset_clip = (2 * x_offset) / (float)m_screenWidth;
+    float yOffset_clip = (2 * y_offset) / (float)m_screenHeight;
+    glm::mat4 transform = glm::translate(
+        glm::mat4(1.0), glm::vec3(xOffset_clip, yOffset_clip, 0));
     transform = glm::inverse(projection * view) * transform * projection * view;
     float x = transform[3][0];
     float y = transform[3][1];
