a34d44c011baceffec3abc95e9b62274d372c54a
xubury <xubury97@gmail.com>
2021-03-16 19:51:06 +0800

    * record real value

--- scripts/test.py
+++ scripts/test.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 scale_factor = 2
 # the grid I used has 50mm distance between neighboring points horizontally and 25mm vertically
 matcher = matching.Matching(2**scale_factor, 50, 25)
-# base_dir = "E:\\recon-image\\1-13\\2\\visual\\scene\\views\\view_0022.mve"
-base_dir = "E:\\recon-image\\11-7\\visual\\4\\scene\\views\\view_0002.mve"
+# base_dir = "E:\\recon-image\\1-13\\2\\visual\\scene\\views\\view_0022.mve"  # real value: 90
+base_dir = "E:\\recon-image\\11-7\\visual\\4\\scene\\views\\view_0001.mve"  # real value: 130
 visual_name = os.path.join(base_dir, "undist-L2.png")
 thermal_name = os.path.join(base_dir, "thermal.jpg")
 dm_name = os.path.join(base_dir, "smvs-visual-B2.mvei")
@@ -21,7 +21,7 @@ thermal_dm_name = os.path.join(base_dir, "smvs-thermal-SGM.mvei")
 output_name = "merged-test.jpg"
 
 
-scales = range(10, 1310, 10)
+scales = range(10, 500, 10)
 bestScale, bestScore, scores = matcher.guessScale(
     visual_name, thermal_name, dm_name, sgm_name, thermal_dm_name, scales)
 
