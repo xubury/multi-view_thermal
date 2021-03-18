4ef0822ecb32646297759b69f51bf98a6e218341
xubury <xubury97@gmail.com>
2021-03-18 14:39:57 +0800

    * run scale estimation before mapping

--- scripts/main.py
+++ scripts/main.py
@@ -8,8 +8,10 @@ import cv2
 from sklearn import linear_model, datasets
 import matplotlib.pyplot as plt
 
-mve_dir = "E:/recon-image/1-13/2/visual/scene/views"
-thermal_dir = "E:/recon-image/1-13/2/thermal"
+mve_dir = "E:/recon-image/11-7/visual/4/scene/views"
+thermal_dir = "E:/recon-image/11-7/thermal/4"
+# mve_dir = "E:/recon-image/1-13/2/visual/scene/views"
+# thermal_dir = "E:/recon-image/1-13/2/thermal"
 scale_factor = 2
 
 mve_entris = img_glob.getMVEEntries(mve_dir)
@@ -21,22 +23,9 @@ t0 = time.time()
 # the grid I used has 50mm distance between neighboring points horizontally and 25mm vertically
 matcher = matching.Matching(2**scale_factor, 50, 25)
 
-for mve_view_dir in mve_entris:
-    for filename in os.listdir(mve_view_dir):
-        if re.search("depth-visual-L" + str(scale_factor) + ".mvei", filename):
-            dm_name = os.path.join(mve_view_dir, filename)
-            visual_name = os.path.join(
-                mve_view_dir, "undist-L" + str(scale_factor) + ".png")
-            thermal_name = os.path.join(mve_view_dir, "thermal.jpg")
-            output_name = os.path.join(mve_view_dir, "merged-mvs.jpg")
-            # 104 converts the depth value unit to mm(millimeter)
-            res = matcher.mapThermalToVisual(
-                visual_name, thermal_name, dm_name, 90)
-            cv2.imwrite(output_name, res)
-            break
-
-X = []
-Y = []
+# estimate best scale
+scales = []
+scores = []
 for mve_view_dir in mve_entris:
     for filename in os.listdir(mve_view_dir):
         if re.search("smvs-visual-B" + str(scale_factor) + ".mvei", filename):
@@ -48,26 +37,55 @@ for mve_view_dir in mve_entris:
             sgm_name = os.path.join(mve_view_dir, "smvs-visual-sgm.mvei")
             thermal_dm_name = os.path.join(
                 mve_view_dir, "smvs-thermal-sgm.mvei")
-            scales = range(10, 300, 5)
-            bestScale, bestScore, scores = matcher.guessScale(
-                visual_name, thermal_name, dm_name, sgm_name, thermal_dm_name, scales)
-            X.append(bestScale)
-            Y.append(bestScore)
+            bestScale, bestScore, _ = matcher.guessScale(
+                visual_name, thermal_name, dm_name, sgm_name, thermal_dm_name, range(10, 300, 5))
+            scales.append(bestScale)
+            scores.append(bestScore)
+            break
+
+# calculate average
+scales = np.array(scales)
+scores = np.array(scores)
+scales = scales.reshape(-1, 1)
+scores = scores.reshape(-1, 1)
+total = np.sum(scores)
+avgScale = 0
+for i in range(len(scales)):
+    avgScale += scores[i] / total * scales[i]
+print("Scale estimation took:", time.time() - t0, "seconds.")
+print("average scale:", avgScale)
+
+t0 = time.time()
+# map the image using the scale
+for mve_view_dir in mve_entris:
+    for filename in os.listdir(mve_view_dir):
+        if re.search("depth-visual-L" + str(scale_factor) + ".mvei", filename):
+            dm_name = os.path.join(mve_view_dir, filename)
+            visual_name = os.path.join(
+                mve_view_dir, "undist-L" + str(scale_factor) + ".png")
+            thermal_name = os.path.join(mve_view_dir, "thermal.jpg")
+            output_name = os.path.join(mve_view_dir, "merged-mvs.jpg")
             # 104 converts the depth value unit to mm(millimeter)
             res = matcher.mapThermalToVisual(
-                visual_name, thermal_name, dm_name, 90)
+                visual_name, thermal_name, dm_name, avgScale)
             cv2.imwrite(output_name, res)
             break
 
-print("Matching took: ", time.time() - t0, "seconds.")
-X = np.array(X)
-Y = np.array(Y)
-X = X.reshape(-1, 1)
-Y = Y.reshape(-1, 1)
-total = np.sum(Y)
-best = 0
-for i in range(len(X)):
-    best += Y[i] / total * X[i]
-print("best", best)
-plt.scatter(X, Y, alpha=0.4, label='类别A')
+for mve_view_dir in mve_entris:
+    for filename in os.listdir(mve_view_dir):
+        if re.search("smvs-visual-B" + str(scale_factor) + ".mvei", filename):
+            dm_name = os.path.join(mve_view_dir, filename)
+            visual_name = os.path.join(
+                mve_view_dir, "undist-L" + str(scale_factor) + ".png")
+            thermal_name = os.path.join(mve_view_dir, "thermal.jpg")
+            output_name = os.path.join(mve_view_dir, "merged-smvs.jpg")
+            # 104 converts the depth value unit to mm(millimeter)
+            res = matcher.mapThermalToVisual(
+                visual_name, thermal_name, dm_name, avgScale)
+            cv2.imwrite(output_name, res)
+            break
+
+print("Matching took:", time.time() - t0, "seconds.")
+
+plt.scatter(scales, scores, alpha=0.4, label='scale-score')
 plt.show()
